02facf14626aaaa1baae717d8dcec6203b164a1e
Eszter Pálmai <eszter.palmai@shoprenter.hu>
2025-03-13 17:24:08 +0100

    Revert "SPP-5646 delete mock components"
    This reverts commit 19998d60b55cad6a9ec91a73f3fee684e3af78fc.

---
+++ src/components/SrSubmitButton/SrSubmitButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-btn v-bind="$attrs" v-on="$listeners" :outlined="false" large color="error">
+    {{ text }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'SrSubmitButton',
+  props:
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      default: 'primary',
+      validator: function(value) {
+        return ['primary', 'secondary', 'success', 'info', 'warning', 'error'].includes(value)
+      }
+    },
+  },
+  inheritAttrs: false
+
+}
+
+</script>
