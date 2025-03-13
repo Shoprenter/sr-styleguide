86b6123d3ea8dae8a55ddd5bd82a62276654b2e7
Eszter Pálmai <eszter.palmai@shoprenter.hu>
2025-03-13 17:21:36 +0100

    SPP-5646 preset structure
    Resolves: SPP-5646

--- build/stylesheet/stylesheet.js
+++ build/stylesheet/stylesheet.js
@@ -1,7 +1,7 @@
 import '../../src/plugins/vuetify/preset/style/css/root_variables.css'
 import '../../src/plugins/vuetify/vuetify.sass'
 import 'vuetify/src/styles/main.sass'
-import '../../src/plugins/vuetify/preset/override.scss'
+import '../../src/plugins/vuetify/preset/overrides.sass'
 
 
 import './component-styles.scss'
