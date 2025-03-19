d8032961a075f0e9ae7db0fce6ffbe0f8a7b3e23
Eszter Pálmai <eszter.palmai@shoprenter.hu>
2025-03-19 09:59:23 +0100

    SPP-5651 wip
    Resolves: SPP-5651

---
+++ src/components/icons/NextIcon.vue
@@ -0,0 +1,11 @@
+<template>
+    <svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0.0688477 1.15443L3.79858 4.88416L0.0688477 8.61389L1.20398 9.74902L6.06885 4.88416L1.20398 0.0192947L0.0688477 1.15443Z" fill="currentColor"/>
+    </svg>
+</template>
+
+<script>
+export default {
+    name: 'NextIcon'
+}
+</script>
---
+++ src/plugins/vuetify/preset/icons/index.js
@@ -0,0 +1,132 @@
+import HelpCircleOutline from '../../../../components/icons/HelpCircleOutlineIcon'
+import SettingIcon from '../../../../components/icons/SettingIcon'
+import MailIcon from '../../../../components/icons/MailIcon'
+import DuplicateIcon from '../../../../components/icons/DuplicateIcon'
+import CheckboxOn from '../../../../components/icons/CheckboxOnIcon'
+import CheckboxOff from '../../../../components/icons/CheckboxOffIcon'
+import CheckboxIndeterminate
+    from '../../../../components/icons/CheckboxIndeterminateIcon'
+import AddCircleOutline
+    from '../../../../components/icons/AddCircleOutlineIcon'
+import ExportIcon from '../../../../components/icons/ExportIcon'
+import ImportIcon from '../../../../components/icons/ImportIcon'
+import MagnifyIcon from '../../../../components/icons/MagnifyIcon'
+import FilterIcon from '../../../../components/icons/FilterIcon'
+import InfoIcon from '../../../../components/icons/InfoIcon'
+import TrashIcon from '../../../../components/icons/TrashIcon'
+import PencilIcon from '../../../../components/icons/PencilIcon'
+import LightningIcon from '../../../../components/icons/LightningIcon'
+import OpenInNewIcon from '../../../../components/icons/OpenInNewIcon'
+import FindIcon from '../../../../components/icons/FindIcon'
+import SuccessIcon from '../../../../components/icons/SuccessIcon'
+import ErrorIcon from '../../../../components/icons/ErrorIcon'
+import RadioOn from '../../../../components/icons/RadioOn'
+import RadioOff from '../../../../components/icons/RadioOff'
+import InformationIcon from '../../../../components/icons/InformationIcon'
+import PackageIcon from '../../../../components/icons/PackageIcon'
+import PadlockOutlineIcon from '../../../../components/icons/PadlockOutlineIcon.vue'
+import WarningCircleOutlineIcon from '../../../../components/icons/WarningCircleOutlineIcon.vue'
+import ErrorCircleOutlineIcon from '../../../../components/icons/ErrorCircleOutlineIcon.vue'
+import SuccessCircleOutlineIcon from '../../../../components/icons/SuccessCircleOutlineIcon.vue'
+
+export default {
+    iconfont: 'mdi',
+    values: {
+        eye: 'mdi-eye',
+        close: 'mdi-close',
+        pencil: {
+            component: PencilIcon
+        },
+        openInNew: {
+            component: OpenInNewIcon
+        },
+        addCircleOutline: {
+            component: AddCircleOutline
+        },
+        helpCircle: 'mdi-help-circle',
+        helpCircleOutline: {
+            component: HelpCircleOutline
+        },
+        arrowUp: 'mdi-arrow-up',
+        checkboxOn: {
+            component: CheckboxOn
+        },
+        checkboxOff: {
+            component: CheckboxOff
+        },
+        checkboxIndeterminate: {
+            component: CheckboxIndeterminate
+        },
+        radioOn: {
+            component: RadioOn
+        },
+        radioOff: {
+            component: RadioOff
+        },
+        arrowDown: 'mdi-arrow-down',
+        cart: 'mdi-cart',
+        trashCan: {
+            component: TrashIcon
+        },
+        dropdown: 'mdi-chevron-down',
+        cancel: 'mdi-close',
+        duplicate: {
+            component: DuplicateIcon
+        },
+        mail: {
+            component: MailIcon
+        },
+        setting: {
+            component: SettingIcon
+        },
+        export: {
+            component: ExportIcon
+        },
+        import: {
+            component: ImportIcon
+        },
+        magnify: {
+            component: MagnifyIcon
+        },
+        filter: {
+            component: FilterIcon
+        },
+        info: {
+            component: InfoIcon
+        },
+        lightning: {
+            component: LightningIcon
+        },
+        mailOutline: 'mdi-email-outline',
+        blankCircle: 'mdi-checkbox-blank-circle',
+        alert: 'mdi-alert',
+        findIcon: {
+            component: FindIcon
+        },
+        srSuccessIcon: {
+            component: SuccessIcon
+        },
+        srErrorIcon: {
+            component: ErrorIcon
+        },
+        informationIcon: {
+            component: InformationIcon
+        },
+        download: 'mdi-download',
+        package: {
+            component: PackageIcon
+        },
+        padlockOutline: {
+            component: PadlockOutlineIcon
+        },
+        warningCircleOutline: {
+            component: WarningCircleOutlineIcon
+        },
+        errorCircleOutline: {
+            component: ErrorCircleOutlineIcon
+        },
+        successCircleOutline: {
+            component: SuccessCircleOutlineIcon
+        }
+    }
+}
