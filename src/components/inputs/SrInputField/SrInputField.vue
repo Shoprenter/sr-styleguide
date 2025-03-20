48aedc87221cbe0644d985bbfb127174f4cdb77b
Eszter Pálmai <eszter.palmai@shoprenter.hu>
2025-03-20 16:52:52 +0100

    SPP-5656 initial
    Resolves: SPP-5656

---
+++ src/components/inputs/SrInputField/SrInputField.vue
@@ -0,0 +1,9 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: 'SrInputField'
+}
+</script>
--- src/plugins/component-library/index.js
+++ src/plugins/component-library/index.js
@@ -1,8 +1,10 @@
 import SrIcons from './library/SrIcons'
+import SrInputField from '../../components/inputs/SrInputField/SrInputField.vue'
 
 export default {
     install (Vue, options = {}) {
         Vue.use(SrIcons)
+        Vue.use(SrInputField)
 
         // eg: Vue.component('SrGlobalSnackBar', SrGlobalSnackBar)
     }
