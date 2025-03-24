3dac744e1b1ab3aad44b8d4656715f2643f6a76a
László Szabó <laszlo.szabo@shoprenter.hu>
2025-03-24 14:22:09 +0100

    SPP-5657 - codeblock

--- demo/src/components/sections/SrStyleGuideFormRow/SrStyleGuideFormRow.vue
+++ demo/src/components/sections/SrStyleGuideFormRow/SrStyleGuideFormRow.vue
@@ -1,17 +1,49 @@
 <template>
   <div class="sg-subsection">
-      <sr-form-row label="This is the input label" help="This is the help text" required>
-          <sr-input-field type="text"></sr-input-field>
+    <v-row class="style-guide-row" no-gutters>
+      <sr-form-row help="This is the help text" label="This is the input label" required>
+        <sr-input-field type="text"></sr-input-field>
       </sr-form-row>
+    </v-row>
+    <v-row class="code-container">
+      <sr-code-block code='<sr-form-row
+     label="This is the input label"
+     help="This is the help text"
+     required>
+    <sr-input-field type="text"></sr-input-field>
+ </sr-form-row>
+'/>
+      <div class="flex">
+        <v-btn icon class="copy-button ml-2" color="primary" @click="copyCode">
+          <sr-duplicate-icon size="20"/>
+        </v-btn>
+        <transition name="fade">
+          <span class="copied-text" v-if="copied">Másolva!</span>
+        </transition>
+      </div>
+    </v-row>
   </div>
 </template>
 
 <script>
 
-import SrStyleGuideItemRow from '../../SrStyleGuideItemRow/SrStyleGuideItemRow.vue'
+import '../../SrStyleGuideItemRow/SrStyleGuideItemRow.scss'
 
 export default {
-    name: 'SrStyleGuideFormRow',
-    components: { SrStyleGuideItemRow }
+  name: 'SrStyleGuideFormRow',
+  data () {
+    return {
+      copied: false
+    }
+  },
+  methods: {
+    copyCode () {
+      navigator.clipboard.writeText(this.code)
+      this.copied = true
+      setTimeout(() => {
+        this.copied = false
+      }, 600)
+    }
+  }
 }
 </script>
