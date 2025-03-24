d9c2133d0faabb3392a753ec6e4dc21bc3c3ae28
Eszter Pálmai <eszter.palmai@shoprenter.hu>
2025-03-24 01:09:30 +0100

    SPP-5655
    Resolves: SPP-5655

--- demo/src/components/sections/SrStyleGuideInputs/SrStyleGuideInputs.vue
+++ demo/src/components/sections/SrStyleGuideInputs/SrStyleGuideInputs.vue
@@ -16,10 +16,10 @@
       <sr-style-guide-item-row
           align="start"
           code="<sr-input-field type='number' hint='This is a hint'/>">
-      <sr-input-field
-          type="number"
-          hint="This is a hint"
-      />
+        <sr-input-field
+            type="number"
+            hint="This is a hint"
+        />
       </sr-style-guide-item-row>
     </div>
     <div class="sg-block">
@@ -27,10 +27,21 @@
       <sr-style-guide-item-row
           align="start"
           code="<sr-input-field type='text' error-messages='Here is an error'/>">
-      <sr-input-field
-          type="text"
-          error-messages="Here is an error"
-      />
+        <sr-input-field
+            type="text"
+            error-messages="Here is an error"
+        />
+      </sr-style-guide-item-row>
+    </div>
+    <div class="sg-block">
+      <h4>Select</h4>
+      <sr-style-guide-item-row
+          align="start"
+          code="&lt;sr-select :items=&quot;[{text: 'Example 1', value: 1}, {text: 'Example 2', value: 2}]&quot; :placeholder='--- Kérlek válassz ---' /&gt;">
+        <sr-select
+            :items="[{text: 'Example 1', value: 1}, {text: 'Example 2', value: 2}]"
+            :placeholder="'--- Kérlek válassz ---'"
+        />
       </sr-style-guide-item-row>
     </div>
   </div>
@@ -40,5 +51,6 @@
 
 export default {
     name: 'StyleGuideInputs'
+
 }
 </script>
