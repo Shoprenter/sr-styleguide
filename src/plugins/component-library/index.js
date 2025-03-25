6688eca5b28e1391c092e3218477a015b66a5d8e
Eszter Pálmai <eszter.palmai@shoprenter.hu>
2025-03-25 08:07:45 +0100

    SPP-5649 move files around
    Resolves: SPP-5649

--- src/plugins/component-library/index.js
+++ src/plugins/component-library/index.js
@@ -2,9 +2,7 @@ import SrIcons from './library/SrIcons'
 import SrButtons from './library/SrButtons'
 import SrInputs from './library/SrInputs'
 import SrFormRow from './library/SrFormRow'
-import SrSlimLayout from '../../components/SrSlimLayout/SrSlimLayout.vue'
-import SrSlimHeading from '../../components/SrSlimLayout/SrSlimHeading.vue'
-import SrSlimContent from '../../components/SrSlimLayout/SrSlimContent.vue'
+import SrLayouts from './library/SrLayouts'
 
 export default {
     install (Vue, options = {}) {
@@ -12,11 +10,6 @@ export default {
         Vue.use(SrButtons)
         Vue.use(SrInputs)
         Vue.use(SrFormRow)
-
-        Vue.component('SrSlimLayout', SrSlimLayout)
-        Vue.component('SrSlimHeading', SrSlimHeading)
-        Vue.component('SrSlimContent', SrSlimContent)
-
-        // eg: Vue.component('SrGlobalSnackBar', SrGlobalSnackBar)
+        Vue.use(SrLayouts)
     }
 }
