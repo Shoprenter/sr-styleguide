c14de2d12b5a3d169cafe61d8e5f25391b95e5f6
Eszter Pálmai <eszter.palmai@shoprenter.hu>
2025-03-25 12:20:49 +0100

    SPP-5664
    Resolves: SPP-5664

---
+++ src/components/inputs/SrCheckbox/SrCheckbox.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-checkbox
+      v-bind="$attrs"
+      v-on="$listeners"
+      hide-details
+      :ripple="false"
+  ></v-checkbox>
+</template>
+
+<script>
+export default {
+    name: 'SrCheckbox',
+    inheritAttrs: false
+}
+</script>
