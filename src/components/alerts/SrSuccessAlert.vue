ec989965f16e2b22a51efcfa395de795cc73c013
Eszter Pálmai <eszter.palmai@shoprenter.hu>
2025-03-27 16:22:41 +0100

    SPP-5668 fix icons
    Resolves: SPP-5668

--- src/components/alerts/SrSuccessAlert.vue
+++ src/components/alerts/SrSuccessAlert.vue
@@ -1,14 +1,16 @@
 <template>
-  <v-alert dismissible prominent type="success" v-bind="$attrs" v-on="$listeners">
-    <template v-slot:prepend>
-      <v-icon class="sr-alert-icon" color="white" size="32">mdi-check-circle</v-icon>
-    </template>
-    <slot/>
+  <v-alert
+      prominent
+      dismissible
+      type="success"
+      v-bind="$attrs"
+      v-on="$listeners"
+      icon="mdi-check-circle">
+    <slot />
   </v-alert>
 </template>
 
 <script>
-import './SrAlerts.scss'
 export default {
     name: 'SrSuccessAlert',
     inheritAttrs: false
