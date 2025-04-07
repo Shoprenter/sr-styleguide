85120ae00cf67b034405c4e3b06e76cce7c1daac
László Szabó <laszlo.szabo@shoprenter.hu>
2025-04-07 13:11:53 +0200

    SPP-5693 - Add examples

--- demo/src/components/sections/SrStyleGuideInputs/SrStyleGuideInputs.vue
+++ demo/src/components/sections/SrStyleGuideInputs/SrStyleGuideInputs.vue
@@ -56,6 +56,9 @@
     </div>
     <div id="simple-multi-checkbox-inputs" class="sg-block">
       <h4 class="text-h4">Simple Multi Checkbox</h4>
+      <p>
+        Use this component when you only need to list the available options without requiring any special logic or custom behavior.
+      </p>
       <v-row>
         <v-col>
           <sr-simple-multi-checkbox
@@ -70,16 +73,11 @@
       </v-row>
       <v-row>
         <v-col cols="6">
-          <sr-code-block code='<sr-multi-checkbox :loading="false">
-   <sr-array-checkbox
-       v-for="option in multiCheckboxOptions"
-       :model-value="selectedOptions"
-       :key="option.value"
-       :label="option.label"
-       @change:modelValue="selectedOptions = $event"
-       :checkbox-value="option.value">
-   </sr-array-checkbox>
- </sr-multi-checkbox>'/>
+          <sr-code-block code='<sr-simple-multi-checkbox
+      :loading="false"
+      :multi-checkbox-options="multiCheckboxOptions"
+      v-model="selectedOptions">
+ </sr-simple-multi-checkbox>'/>
         </v-col>
         <v-col cols="6">
           <sr-code-block language="JavaScript" code="data () {
@@ -99,17 +97,29 @@
     </div>
     <div id="multi-checkbox-inputs" class="sg-block">
       <h4 class="text-h4">Multi Checkbox</h4>
+      <p>
+        Use this component when the options require unique behavior—for example, if some options should not be selectable, must always be selected, or if selecting certain items should trigger additional events or logic.
+      </p>
       <v-row>
         <v-col>
         <sr-multi-checkbox :loading="false">
           <sr-array-checkbox
-              v-for="option in multiCheckboxOptions"
               :model-value="selectedOptions"
-              :key="option.value"
-              :label="option.label"
+              label="Option 1 (Disabled)"
               @change:modelValue="selectedOptions = $event"
-              :checkbox-value="option.value">
-          </sr-array-checkbox>
+              disabled="disabled"
+              :checkbox-value="1"/>
+          <sr-array-checkbox
+              :model-value="selectedOptions"
+              label="Option 2 (Aways selected)"
+              @change:modelValue="selectedOptions = $event"
+              disabled="disabled"
+              :checkbox-value="2"/>
+          <sr-array-checkbox
+              :model-value="selectedOptions"
+              label="Option 3"
+              @change:modelValue="selectedOptions = $event"
+              :checkbox-value="3"/>
         </sr-multi-checkbox>
         </v-col>
         <v-col>
@@ -119,26 +129,28 @@
       <v-row>
         <v-col cols="6">
           <sr-code-block code='<sr-multi-checkbox :loading="false">
-   <sr-array-checkbox
-       v-for="option in multiCheckboxOptions"
-       :model-value="selectedOptions"
-       :key="option.value"
-       :label="option.label"
-       @change:modelValue="selectedOptions = $event"
-       :checkbox-value="option.value">
-   </sr-array-checkbox>
+    <sr-array-checkbox
+        :model-value="selectedOptions"
+        label="Option 1 (Disabled)"
+        @change:modelValue="selectedOptions = $event"
+        disabled="disabled"
+        :checkbox-value="1"/>
+    <sr-array-checkbox
+        :model-value="selectedOptions"
+        label="Option 2 (Aways selected)"
+        @change:modelValue="selectedOptions = $event"
+        disabled="disabled"
+        :checkbox-value="2"/>
+    <sr-array-checkbox
+        :model-value="selectedOptions"
+        label="Option 3"
+        @change:modelValue="selectedOptions = $event"
+        :checkbox-value="3"/>
  </sr-multi-checkbox>'/>
         </v-col>
         <v-col cols="6">
           <sr-code-block language="JavaScript" code="data () {
       return {
-          multiCheckboxOptions: [
-            {label: 'Option 1', value: 1},
-            {label: 'Option 2', value: 2},
-            {label: 'Option 3', value: 3},
-            {label: 'Option 4', value: 4},
-            {label: 'Option 5', value: 5},
-          ],
           selectedOptions: [2, 3]
       }
 }"/>
