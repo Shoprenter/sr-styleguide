c5c67e26ee33240a4de821b2cce4cfa3849930d1
László Szabó <laszlo.szabo@shoprenter.hu>
2025-04-07 11:57:36 +0200

    SPP-5693 - Refactoring - extract common parts and tests

---
+++ src/components/inputs/SrMultiCheckbox/BaseMultiCheckbox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="sr-multi-checkbox">
+    <div class="checkbox-container" ref="checkboxWrapper">
+      <template v-if="loading">
+        <div class="preloader-container">
+          <v-progress-circular
+              class="text-center"
+              color="primary"
+              indeterminate
+              size="38"
+          />
+        </div>
+      </template>
+      <template v-else>
+        <slot name="checkboxes"/>
+      </template>
+    </div>
+    <div class="multi-checkbox-actions">
+      <sr-primary-button text data-test-id="selectAllCheckbox" @click="selectAllCheckboxes">
+        {{ $vuetify.lang.t('$vuetify.selectAll') }}
+      </sr-primary-button>
+      <span class="multi-checkbox-separator">/</span>
+      <sr-primary-button text data-test-id="unSelectAllCheckbox" @click="unSelectAllCheckboxes">
+        {{ $vuetify.lang.t('$vuetify.clearAll') }}
+      </sr-primary-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { MultiCheckboxMixin } from './mixin/SrMultiCheckboxMixin';
+
+export default {
+  name: 'BaseMultiCheckbox',
+  mixins: [MultiCheckboxMixin],
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
