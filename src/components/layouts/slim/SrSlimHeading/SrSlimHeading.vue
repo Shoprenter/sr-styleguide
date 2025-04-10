f2b2c2a7661d688a98add9cc22e96750b5159c8f
László Szabó <laszlo.szabo@shoprenter.hu>
2025-04-10 14:06:53 +0200

    SPP-5673 - fix heading margin

--- src/components/layouts/slim/SrSlimHeading/SrSlimHeading.vue
+++ src/components/layouts/slim/SrSlimHeading/SrSlimHeading.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="sr-heading d-flex w-100 justify-space-between mb-4">
+  <div class="sr-heading d-flex w-100 justify-space-between">
     <h1 class="text-h1">{{ title }}</h1>
     <div>
       <slot></slot>
